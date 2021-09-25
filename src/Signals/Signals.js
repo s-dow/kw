18f6cfff2987a3d3b6da58f4952c8e405850d03a
s-dow <40843634+s-dow@users.noreply.github.com>
2021-09-24 22:11:15 -0400

    9/24

--- src/Signals/Signals.js
+++ src/Signals/Signals.js
@@ -11,6 +11,26 @@ export const Signals = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [signalsPerPage] = useState(10);
 
+  (function () {
+    var cors_api_host = "cors-anywhere.herokuapp.com";
+    var cors_api_url = "https://" + cors_api_host + "/";
+    var slice = [].slice;
+    var origin = window.location.protocol + "//" + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function () {
+      var args = slice.call(arguments);
+      var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+      if (
+        targetOrigin &&
+        targetOrigin[0].toLowerCase() !== origin &&
+        targetOrigin[1] !== cors_api_host
+      ) {
+        args[1] = cors_api_url + args[1];
+      }
+      return open.apply(this, args);
+    };
+  })();
+
   useEffect(() => {
     (async () => {
       const getCookie = (cname) => {
