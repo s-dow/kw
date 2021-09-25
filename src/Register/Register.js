b3bed6556c4c8851a8a2bf64e7ddaf04feb2c08e
s-dow <40843634+s-dow@users.noreply.github.com>
2021-09-24 21:13:52 -0400

    fix

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -27,9 +27,7 @@ export const Register = () => {
           authCode: env.REACT_APP_AUTH_CODE,
         }),
       }
-    );
-
-    const data = await response.json().then((res) => {
+    ).then((res) => {
       function setCookie(cname, cvalue, exdays) {
         const d = new Date();
         d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
@@ -43,10 +41,10 @@ export const Register = () => {
         let ca = document.cookie.split(";");
         for (let i = 0; i < ca.length; i++) {
           let c = ca[i];
-          while (c.charAt(0) == " ") {
+          while (c.charAt(0) === " ") {
             c = c.substring(1);
           }
-          if (c.indexOf(name) == 0) {
+          if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
           }
         }
