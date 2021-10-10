6ed6f5cb4352962a08a31652ad9cfc596ae35d1d
s-dow <40843634+s-dow@users.noreply.github.com>
2021-10-09 21:30:24 -0400

    proxy

--- src/Signals/Signals.js
+++ src/Signals/Signals.js
@@ -29,25 +29,25 @@ export const Signals = () => {
       }
       const token = await getCookie("token");
 
-      (function () {
-        var cors_api_host = "calm-brook-48240.herokuapp.com";
-        var cors_api_url = "https://" + cors_api_host + "/";
-        var slice = [].slice;
-        var origin = window.location.protocol + "//" + window.location.host;
-        var open = XMLHttpRequest.prototype.open;
-        XMLHttpRequest.prototype.open = function () {
-          var args = slice.call(arguments);
-          var targetOrigin = /^https?:\/\/([^]+)/i.exec(args[1]);
-          if (
-            targetOrigin &&
-            targetOrigin[0].toLowerCase() !== origin &&
-            targetOrigin[1] !== cors_api_host
-          ) {
-            args[1] = cors_api_url + args[1];
-          }
-          return open.apply(this, args);
-        };
-      })();
+      // (function () {
+      //   var cors_api_host = "calm-brook-48240.herokuapp.com";
+      //   var cors_api_url = "https://" + cors_api_host + "/";
+      //   var slice = [].slice;
+      //   var origin = window.location.protocol + "//" + window.location.host;
+      //   var open = XMLHttpRequest.prototype.open;
+      //   XMLHttpRequest.prototype.open = function () {
+      //     var args = slice.call(arguments);
+      //     var targetOrigin = /^https?:\/\/([^]+)/i.exec(args[1]);
+      //     if (
+      //       targetOrigin &&
+      //       targetOrigin[0].toLowerCase() !== origin &&
+      //       targetOrigin[1] !== cors_api_host
+      //     ) {
+      //       args[1] = cors_api_url + args[1];
+      //     }
+      //     return open.apply(this, args);
+      //   };
+      // })();
 
       (async () => {
         const response = await fetch(
